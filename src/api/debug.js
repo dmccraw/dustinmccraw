fd128fd7101fa8ce0e9547327ad48eb738bb009c
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2022-10-11 14:30:45 -0700

    change test

--- src/api/debug.js
+++ src/api/debug.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   res.header(`x-site-id`, `dustins-site`)
   res.header(`x-build-id`, `dustins-build-id`)
   res.header(`x-function-id`, `dustins-function-id`)
-  res.header(`x-dustin-test`, `mccraw`)
+  res.header(`x-dustin-test`, `mccraw123`)
   res.status(200).send({
     host: req.host,
     hostname: req.hostname,
