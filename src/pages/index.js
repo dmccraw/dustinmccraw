91c07daea58399bb63f804284c8eae5ff6ae13e5
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2022-11-16 13:29:52 -0800

    create pr

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   const [time, setTime] = useState(``)
+  const i = 0
   useEffect(() => {
     const interval = setInterval(async () => {
       const res = await fetch(`/api/time`, {
